e6f0cf0c9e190d583531cae1555971b4a6da11a8
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-05-28 17:57:39 +0530

    basic structure and functionality completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,59 +1,62 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { SearchBox } from "../modals/SearchBox";
 
 export const Home = () => {
+    const [query, setQuery] = useState("good");
+    const [index, setIndex] = useState(0);
+    const [length, setLength] = useState("");
+
+    const handleChange = (e) => {
+        setQuery(e.target.value);
+    };
+
+    const prev = () => {
+        setIndex((prev) => prev - 1);
+    };
+
+    const next = () => {
+        setIndex((prev) => prev + 1);
+    };
+
+    const bodyRef = useRef(document.body);
+
+    const highlightMatchingText = () => {
+        const allTextElements = bodyRef.current.querySelectorAll("h1, h2, h3, h4, h5, h6, p, a");
+        allTextElements.forEach((element, i) => {
+            const innerText = element.innerText;
+
+            console.log(index);
+
+            const highightedLetters = innerText.replace(
+                query,
+                i === index
+                    ? `<span class="red">${query}</span>`
+                    : `<span class="yellow">${query}</span>`
+            );
+            element.innerHTML = highightedLetters;
+        });
+    };
+
+    useEffect(() => {
+        highlightMatchingText(index);
+    }, [query, index]);
+
     return (
-        <section className="py-[80px]">
+        <section className="py-[80px] relative">
+            <SearchBox
+                query={query}
+                setQuery={setQuery}
+                handleChange={handleChange}
+                prev={prev}
+                next={next}
+            />
             <section className="wrapper">
-                <p className="text-justify leading-8">
-                    World War II: The Global Conflict that Shaped the World Introduction: World War
-                    II (WWII) was a global military conflict that lasted from 1939 to 1945. It
-                    involved many nations from various regions, making it the deadliest war in human
-                    history. Fought on multiple fronts across the globe, WWII had far-reaching
-                    consequences, reshaping political, economic, and social landscapes worldwide. In
-                    this article, we will delve into the key events, causes, and outcomes of World
-                    War II, examining its profound impact on the course of history. 1. Causes of
-                    World War II: 1.1 Treaty of Versailles and the Rise of Nazism: The harsh terms
-                    imposed on Germany by the Treaty of Versailles after World War I created
-                    economic turmoil and resentment, providing fertile ground for the rise of Adolf
-                    Hitler and the Nazi Party in Germany. 1.2 Aggressive Expansionism: Aggressive
-                    expansionist policies pursued by fascist regimes, such as Nazi Germany, Fascist
-                    Italy, and Imperial Japan, sought to extend their territories, influence, and
-                    control, leading to territorial disputes and heightening global tensions. 1.3
-                    Failure of Appeasement: The policy of appeasement pursued by Western powers,
-                    attempting to avoid conflict by accommodating aggressive actions, ultimately
-                    failed in deterring the ambitions of expansionist powers, emboldening their
-                    aggressive behavior. 2. Key Events and Turning Points: 2.1 German Invasion of
-                    Poland: On September 1, 1939, Germany launched a blitzkrieg invasion of Poland,
-                    triggering the start of WWII. This act of aggression led to the declaration of
-                    war by Britain and France on Germany. 2.2 Battle of Stalingrad: The Battle of
-                    Stalingrad (1942-1943) marked a significant turning point in the war. The Soviet
-                    Union successfully defended the city against the German forces, inflicting heavy
-                    casualties and dealing a major blow to Hitler's military ambitions. 2.3 D-Day
-                    and the Normandy Invasion: On June 6, 1944, Allied forces launched a massive
-                    amphibious invasion on the beaches of Normandy, France. Known as D-Day, this
-                    operation opened a second front in Europe and marked a crucial step towards the
-                    liberation of Western Europe from German occupation. 2.4 Pacific Theater and the
-                    Battle of Midway: The Battle of Midway (1942) was a decisive naval battle
-                    between the United States and Japan. The U.S. victory halted Japanese expansion
-                    in the Pacific and turned the tide of the war in favor of the Allies. 3.
-                    Holocaust and War Crimes: The Holocaust, the systematic genocide of six million
-                    Jews by Nazi Germany, stands as one of the most horrific crimes in human
-                    history. The war also witnessed numerous war crimes, including atrocities
-                    committed by Japanese forces in Nanking, China, and the Soviet Union's Katyn
-                    Massacre. 4. Allied Powers and Axis Powers: 4.1 Allied Powers: The Allies
-                    consisted of the United States, the Soviet Union, the United Kingdom, and other
-                    nations. They formed a coalition to combat the Axis Powers, working together to
-                    achieve victory in Europe and the Pacific. 4.2 Axis Powers: The Axis Powers
-                    comprised Germany, Italy, and Japan, along with their smaller allies. They
-                    sought to establish hegemony and expand their territories, leading to conflict
-                    with the Allied Powers. 5. Technological Advances and Warfare: World War II
-                    witnessed significant advancements in military technology. Innovations such as
-                    radar, aircraft carriers, tanks, and jet engines transformed the nature of
-                    warfare, increasing its scale and intensity. 6. Impact and Aftermath: 6.1 Human
-                    Casualties and Devastation: WWII resulted in immense human suffering and loss of
-                    life, with an estimated 70-85 million fatalities, including civilian and
-                    military personnel. Entire cities were reduced to rubble,
-                </p>
+                <p>good sample good morning text hi hello how good sample</p>
+                <h1> sample</h1>
+                <h1> sample</h1>
+                <h1> sample</h1>
+                <h2> sample</h2>
+                <div className="text-justify leading-8 text-red-700 bg-slate-700 h-[100px]"></div>
             </section>
         </section>
     );
--- src/modals/SearchBox.jsx
+++ src/modals/SearchBox.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 
-export const SearchBox = ({ query, setQuery }) => {
-    const handleChange = (e) => {
-        setQuery(e.target.value);
-    };
+export const SearchBox = ({ query, handleChange, prev, next }) => {
     return (
-        <div className="max-w-[360px] w-[100%] absolute top-[20px] right-[100px] z-10 rounded-[5px] overflow-hidden p-[6px] bg-[gray]">
-            <div className="flex items-center text-[#fff]">
+        <div className="max-w-[360px] w-[100%] absolute top-[20px] right-[15%] z-10 rounded-[5px] overflow-hidden p-[6px] bg-[gray]">
+            <div className="flex items-center gap-[10px] text-[#fff]">
                 <div className="left flex justify-between items-center w-[60%] px-[6px] gap-[6px]">
                     <input
-                        className="flex"
+                        className="flex bg-[gray]"
                         type="text"
                         placeholder="find word"
                         onChange={handleChange}
@@ -18,9 +15,9 @@ export const SearchBox = ({ query, setQuery }) => {
                     <span className="inline-block h-[100%] w-[80px]">1/65</span>
                 </div>
 
-                <div className="right flex justify-between items-center w-[40%] px-[6px] gap-[6px]">
-                    <button>{">"}</button>
-                    <button>{"<"}</button>
+                <div className="right flex justify-end items-center w-[40%] px-[6px] gap-[15px]">
+                    <button onClick={next}>{">"}</button>
+                    <button onClick={prev}>{"<"}</button>
                     <button>{"x"}</button>
                 </div>
             </div>
